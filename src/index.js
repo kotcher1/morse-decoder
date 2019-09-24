0a40ee64965e9aee8d27a7a0e7a2e7ff6ac29d38
kotcher1 <43149448+kotcher1@users.noreply.github.com>
2019-09-24 20:30:22 +0200

    Update index.js

--- src/index.js
+++ src/index.js
@@ -38,7 +38,40 @@ const MORSE_TABLE = {
 };
 
 function decode(expr) {
-    // write your solution here
+  let morse_str = '';
+
+  for (let i = 0; i < expr.length; i += 2) {
+
+    if (expr.substr(i, 2) === '10') {
+      morse_str = morse_str + '.';
+    } else if (expr.substr(i, 2) === '11') {
+      morse_str = morse_str + '-';
+    } else if (expr.substr(i, 2) === '00') {
+      morse_str = morse_str + 'n';
+    } else {
+      morse_str = morse_str + ' ';
+    }
+  }
+
+  let letter_str = '';
+
+  for (let i = 0; i < morse_str.length; i += 5) {
+
+    let key_str = '';
+    let morse_letter = morse_str.substring(i, i + 5);
+
+    if (morse_letter.charAt(0) === 'n') {
+
+      morse_letter = morse_letter.replace(/n/g, '');
+      letter_str = letter_str + MORSE_TABLE[morse_letter];
+    } else if (morse_letter.charAt(0) === ' ') {
+      morse_letter = morse_letter.replace('     ', ' ');
+      letter_str = letter_str + morse_letter;
+    } else {
+      letter_str = letter_str + MORSE_TABLE[morse_letter];
+    }
+  }
+  return (letter_str);
 }
 
 module.exports = {
